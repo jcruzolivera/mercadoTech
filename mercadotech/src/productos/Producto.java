f7ec20d6d2c28261b9b9dc37f6950bc1b44e7c9e
jcruzolivera <jolivera317@gmail.com>
2023-06-11 12:56:54 -0300

    add color attr to producto

--- mercadotech/src/productos/Producto.java
+++ mercadotech/src/productos/Producto.java
@@ -2,9 +2,18 @@ package productos;
 
 public class Producto {
 	private String nombre;
+	private String color;
 	
 	public Producto(String nombre){
 		this.nombre = nombre;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
 	}
 	
 	public String getNombre() {
